b342bb46ea8f1d919a73a3d6b412cbb0f6d93546
Martins Ngene <martinsngene9@gamil.com>
2021-09-01 23:55:41 +0100

    Modified files.

--- js/app.js
+++ js/app.js
@@ -1,59 +1,57 @@
 
+const p1 = {
 
-const p1Display = document.querySelector('#p1Display');
+	score: 0,
+	button: document.querySelector('#p1Button'),
+	display: document.querySelector('#p1Display')
+}
 
-const p2Display = document.querySelector('#p2Display');
+const p2 = {
 
-const p1Button = document.querySelector('#p1Button');
+	score: 0,
+	button: document.querySelector('#p2Button'),
+	display: document.querySelector('#p2Display')
+}
 
-const p2Button = document.querySelector('#p2Button');
 
 const resetButton = document.querySelector('#reset');
 
 const winningScoreSelect = document.querySelector('#playTo');
 
-let p1Score = 0;
-
-let p2Score = 0;
 
 let winningScore = 2;
 
 let isGameOver = false;
 
 
-p1Button.addEventListener('click', ()=>{
+function updateScores(player, opponent){
 
 	if(!isGameOver){
 
-		p1Score += 1;
+		player.score += 1;
 
-		if (p1Score === winningScore) {
+		if (player.score === winningScore) {
 			isGameOver = true;
-			p1Display.classList.add('winner');
-			p2Display.classList.add('loser');
+			player.display.classList.add('has-text-success');
+			opponent.display.classList.add('has-text-danger');
+			player.button.disabled = true;
+			opponent.button.disabled = true;
 		}
 
-		p1Display.textContent = p1Score;
+		player.display.textContent = player.score;
 	}
 
+}
+
+p1.button.addEventListener('click', ()=>{
+
+	updateScores(p1, p2)
 });
 
 
-p2Button.addEventListener('click', ()=>{
+p2.button.addEventListener('click', ()=>{
 
-	if(!isGameOver){
-
-		p2Score += 1;
-
-		if (p2Score === winningScore) {
-			isGameOver = true;
-			p1Display.classList.add('loser');
-			p2Display.classList.add('winner');
-		}
-
-		p2Display.textContent = p2Score;
-	}
-
+	updateScores(p2, p1)
 });
 
 winningScoreSelect.addEventListener('change', function (){
@@ -66,14 +64,12 @@ resetButton.addEventListener('click', reset);
 
 function reset(){
 	isGameOver = false;
-	
-	p1Score = 0;
+	for(let p of [p1, p2]){
 
-	p2Score = 0;
+		p.score = 0;
+		p.display.textContent = '0';
+		p.display.classList.remove('has-text-success', 'has-text-danger');
+		p.button.disabled = false;
+	}
 
-	p1Display.textContent = '0';
-
-	p2Display.textContent = '0';
-	p1Display.classList.remove('winner', 'loser');
-	p2Display.classList.remove('winner', 'loser');
 }
